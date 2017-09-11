be84aa307fda3ac4abe5642bffbdad37d18db05d
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 15:34:58 +0200

    feat(mile_to_km_converter): done

--- week-02/day-01/mile_to_km_converter.py
+++ week-02/day-01/mile_to_km_converter.py
@@ -1,2 +1,4 @@
 # Write a program that asks for an integer that is a distance in kilometers,
 # then it converts that value to miles and prints it
+km = float(input("How many km? "))
+print(km*0.62)
