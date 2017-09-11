139bf1bc2bc2ca673f84070013eebee37e852d7b
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 16:11:09 +0200

    feat(odd_even): done

--- week-02/day-01/odd_even.py
+++ week-02/day-01/odd_even.py
@@ -1,2 +1,8 @@
 # Write a program that reads a number form the standard input,
 # Than prints "Odd" if the number is odd, or "Even" it it is even.
+x = int(input("Write a number: "))
+
+if x % 2 == 0:
+    print("Even")
+elif x % 2 != 0:
+    print("Odd")
