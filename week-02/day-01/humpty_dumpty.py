2a63bea25a3ba8a99b5a1fd1db086fe0061fc99f
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 12:05:17 +0200

    feat(w-02_day-01): exam02 - done

--- week-02/day-01/humpty_dumpty.py
+++ week-02/day-01/humpty_dumpty.py
@@ -1,6 +1,16 @@
 # Modify this program to print Humpty Dumpty riddle correctly
-print("All the king's horses and all the king's men")
-print("Humpty Dumpty had a great fall.")
-print("Humpty Dumpty sat on a wall,")
-print("Couldn't put Humpty together again.")
+lineOne = "Humpty Dumpty sat on a wall,"
+lineTwo = "Humpty Dumpty had a great fall."
+lineThree = "All the king's horses and all the king's men"
+lineFour = "Couldn't put Humpty together again."
+
+print(lineOne)
+print(lineTwo)
+print(lineThree)
+print(lineFour)
+
+# print("All the king's horses and all the king's men")
+# print("Humpty Dumpty had a great fall.")
+# print("Humpty Dumpty sat on a wall,")
+# print("Couldn't put Humpty together again.")
 
