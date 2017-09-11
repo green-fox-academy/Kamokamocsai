e4bd34616151fb827fe17bf0434bbb8eca54e7ca
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 16:18:04 +0200

    feat(one_two_a_lot): done

--- week-02/day-01/one_two_a_lot.py
+++ week-02/day-01/one_two_a_lot.py
@@ -3,3 +3,13 @@
 # If the number is one it should print: One
 # If the number is two it should print: Two
 # If the number is more than two it should print: A lot
+x = int(input("1 or 2: "))
+
+if x <= 0:
+    print("Not enough")
+elif x == 1:
+    print("One")
+elif x == 2:
+    print("Two")
+elif x > 2:
+    print("A lot")
