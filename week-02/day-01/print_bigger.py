e5a12d98da7ae9fa9ea43af3688ae26f2014e034
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 16:21:59 +0200

    feat(print_bigger): done

--- week-02/day-01/print_bigger.py
+++ week-02/day-01/print_bigger.py
@@ -1,1 +1,8 @@
 # Write a program that asks for two numbers and prints the bigger one
+x = int(input("pick a number: "))
+y = int(input("pick another number: "))
+
+if x > y:
+    print(x)
+else:
+    print(y)
