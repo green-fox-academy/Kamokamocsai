b92fe5ab3a8472905762c54d66be3ff3e270aca6
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 16:05:48 +0200

    feat(average_of_input): fixed

--- week-02/day-01/average_of_input.py
+++ week-02/day-01/average_of_input.py
@@ -2,3 +2,11 @@
 # then it should print the sum and the average of these numbers like:
 #
 # Sum: 22, Average: 4.4
+a = int(input("Pls write the first number: "))
+b = int(input("Pls write the second number: "))
+c = int(input("Pls write the third number: "))
+d = int(input("Pls write the 4th number: "))
+e = int(input("Pls write the 5th number: "))
+
+sum = float((a + b + c + d + e) / 5)
+print(float(sum))
