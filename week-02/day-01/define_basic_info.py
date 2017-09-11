f53369bf0d7d3fb9c4d8563bec0b3e05798743c0
András Kamocsai <kamo.andras@gmail.com>
2017-09-11 14:32:31 +0200

    feat(define_basic_info.py): solved fixed to print

--- week-02/day-01/define_basic_info.py
+++ week-02/day-01/define_basic_info.py
@@ -1,5 +1,14 @@
 # Define several things as a variable than print their values
 # Your name as a string
+name = "Kamo"
 # Your age as an integer
-# Your height in meters as a double
+age = 34
+# Your height in meters
+height = 1.72
 # Wether you are married or not as a boolean
+married = True
+
+print(name)
+print(age)
+print(height)
+print(married)
