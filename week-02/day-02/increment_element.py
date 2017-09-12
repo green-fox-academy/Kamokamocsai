c00e8a2c16ed55abeab591fe6d1c291820a4505b
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 13:58:36 +0200

    feat(increment_element): done

---
+++ week-02/day-02/increment_element.py
@@ -0,0 +1,8 @@
+# - Create a variable named `t`
+#   with the following content: `[1, 2, 3, 4, 5]`
+# - Increment the third element
+# - Print the third element
+
+t = [1, 2, 3, 4, 5]
+del(t[2])
+print(t[2])
