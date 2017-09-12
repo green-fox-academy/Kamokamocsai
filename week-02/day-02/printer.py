4cb692437a750123547ee55ebab3e05f6c7ca5a4
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 12:39:08 +0200

    feat(printer): done

---
+++ week-02/day-02/printer.py
@@ -0,0 +1,10 @@
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
+
+def printer(a):
+    print(a)
+    
+
+
+printer([23,35])
