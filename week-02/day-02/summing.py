d1fa2e0aa08b245b586da1b156966f27eef568d1
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 11:49:33 +0200

    fix(summing): deleted the white spaces

--- week-02/day-02/summing.py
+++ week-02/day-02/summing.py
@@ -2,11 +2,11 @@
 #   until the given parameter
 
 
-# def listsum(num_list):
-#     the_sum = 0
-#     for i in num_list:
-#         the_sum += i
-#     return the_sum
+def listsum(num_list):
+    the_sum = 0
+    for i in num_list:
+        the_sum += i
+    return the_sum
 
-# print(listsum([1,3,5,7,9]))
-print(sum([1,2,3,4,5]))
+print(listsum([1,3,5,7,9]))
+# print(sum([1,2,3,4,5]))
