080a6e453e03b051ee717b8f50f11e5f781bdf2e
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 17:06:11 +0200

    feat(reverse): done

---
+++ week-02/day-02/reverse.py
@@ -0,0 +1,12 @@
+# - Create a variable named `aj`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Reverse the order of the elements in `aj`
+# - Print the elements of the reversed `aj`
+
+aj = [3, 4, 5, 6, 7]
+
+def revers(list_of_nums):
+    list_of_nums.reverse()
+    return list_of_nums
+
+print(revers(aj))
