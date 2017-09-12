25802b6098b4e71b122aef61071dae8d698cdaa1
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 17:31:50 +0200

    fix(doubling): removed unused lines

--- week-02/day-02/doubling.py
+++ week-02/day-02/doubling.py
@@ -1,8 +1,8 @@
 # - Create a variable named `ak` and assign the value `123` to it
-ak = 123
 # - Create a function called `doubling` that doubles it's input parameter
+# - Print the result of `doubling(ak)`
+ak = 123
 def doubling(x):
     return x * 2
-# - Print the result of `doubling(ak)`
+
 print(doubling(ak))
-print(doubling(200))
