ebc0ad1985c2dbe52ddb5c5a73a1518417bae4e4
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 11:09:32 +0200

    feat(greet and doubling): done

---
+++ week-02/day-02/doubling.py
@@ -0,0 +1,8 @@
+# - Create a variable named `ak` and assign the value `123` to it
+ak = 123
+# - Create a function called `doubling` that doubles it's input parameter
+def doubling(x):
+    return x * 2
+# - Print the result of `doubling(ak)`
+print(doubling(ak))
+print(doubling(200))
---
+++ week-02/day-02/greet.py
@@ -0,0 +1,13 @@
+# - Create variable named `al` and assign the value `Greenfox` to it
+# - Create a function called `greet` that greets it's input parameter
+#     - Greeting is printing e.g. `Greetings, dear Greenfox`
+# - Greet `al`
+
+al = "Greenfox"
+el = "asd"
+def greet(name):
+    print("Greetings, dear " + name)
+    return
+
+greet(al)
+greet(el)
