f5a92eff85214aa0fc31e2b29eff81c4b188ffbb
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 17:27:51 +0200

    feat(factorio): added user input

--- week-02/day-02/factorio.py
+++ week-02/day-02/factorio.py
@@ -1,10 +1,13 @@
 
 # - Create a function called `factorio`
 #   that returns it's input's factorial
+
+number = int(input("Please add a number: "))
+
 def factorio(num):
     for i in range(1, num):
         num = num * i
     return num
 
 
-print(factorio(5))  
+print(factorio(number))  
