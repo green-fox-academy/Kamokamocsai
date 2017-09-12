f3beafdd4ca24e5ac098c59f7f9041025b0e3048
András Kamocsai <kamo.andras@gmail.com>
2017-09-12 11:48:05 +0200

    feat(append and summing): done

---
+++ week-02/day-02/append.py
@@ -0,0 +1,10 @@
+# - Create a variable named `am` and assign the value `kuty` to it
+# - Write a function called `appendA` that gets a string as an input
+#   and appends an 'a' character to its end
+# - Print the result of `appendA(am)`
+
+am = "kuty"
+def appendA(input):
+    print(input + 'a')
+    return
+appendA(am)
---
+++ week-02/day-02/summing.py
@@ -0,0 +1,12 @@
+# - Write a function called `sum` that sum all the numbers
+#   until the given parameter
+
+
+# def listsum(num_list):
+#     the_sum = 0
+#     for i in num_list:
+#         the_sum += i
+#     return the_sum
+
+# print(listsum([1,3,5,7,9]))
+print(sum([1,2,3,4,5]))
