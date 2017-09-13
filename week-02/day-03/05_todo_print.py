34d689195073934d84080562bb7dcf0c7dd8ec46
András Kamocsai <kamo.andras@gmail.com>
2017-09-13 14:01:45 +0200

    feat(05_todo-print): not solved

---
+++ week-02/day-03/05_todo_print.py
@@ -0,0 +1,16 @@
+# Add "My todo:" to the beginning of the todoText
+# Add " - Download games" to the end of the todoText
+# Add " - Diablo" to the end of the todoText but with indention
+
+# Expected outpt:
+
+# My todo:
+#  - Buy milk
+#  - Download games
+#      - Diablo
+
+todoText = " - Buy milk\n"
+
+
+print("My todo:\n" + todoText + " - Download games\n" + "     - Diablo")
+
