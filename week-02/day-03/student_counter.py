6eacb0ecbea35f610f30049b7ccd5f8c3712faf9
András Kamocsai <kamo.andras@gmail.com>
2017-09-13 15:06:22 +0200

    feat(student_counter): sum candies

---
+++ week-02/day-03/student_counter.py
@@ -0,0 +1,23 @@
+# create a function that takes a list of students and prints: 
+# - how many candies are owned by students
+
+# create a function that takes a list of students and prints:
+# - Sum of the age of people who have lass than 5 candies
+
+students = [
+        {'name': 'Teodor', 'age': 3, 'candies': 2},
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Olaf', 'age': 12, 'candies': 7},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+]
+
+def all_candies(list_of_students):
+    all_candies = 0
+    for student in list_of_students:
+        all_candies += student['candies']
+    print("The students has " + str(all_candies) + " candies.")
+
+all_candies(students)
+
