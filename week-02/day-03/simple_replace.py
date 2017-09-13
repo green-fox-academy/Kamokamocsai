94c33e1a879562748682baf2ec70b40874a4d55c
András Kamocsai <kamo.andras@gmail.com>
2017-09-13 11:35:37 +0200

    fix(simple_replace): fixed unused lines

--- week-02/day-03/simple_replace.py
+++ week-02/day-03/simple_replace.py
@@ -1,9 +1,7 @@
-example = "In a dishwasher far far away"
-
 # I would like to replace "dishwasher" with "galaxy" in this example
 # Please fix it for me!
 # Expected ouput: In a galaxy far far away
 
+example = "In a dishwasher far far away"
 example = example.replace("dishwasher", "galaxy")
-
 print(example)
