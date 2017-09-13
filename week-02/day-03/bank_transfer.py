1c1bffe8eac0d88adbd991a54d6ee608aad1a015
András Kamocsai <kamo.andras@gmail.com>
2017-09-13 15:57:40 +0200

    feat(bank_transfer): half way

---
+++ week-02/day-03/bank_transfer.py
@@ -0,0 +1,24 @@
+# Create function that returns the name and balance of cash on an account
+
+# Create function that transfers an amount of cash from one account to another
+# it should have three parameters:
+#  - from account_number
+#  - to account_number
+#  - amount to transfer
+#
+# Print "404 - account not found" if any of the account numbers don't exist
+
+accounts = [
+	{ 'client_name': 'Igor', 'account_number': 11234543, 'balance': 203004099.2 },
+	{ 'client_name': 'Vladimir', 'account_number': 43546731, 'balance': 5204100071.23 },
+	{ 'client_name': 'Sergei', 'account_number': 23456311, 'balance': 1353600.0 }
+]
+
+print(accounts[0]['client_name'] + " : " + str(accounts[0]['balance']))
+
+
+# def name_balance(acc):
+#     for client in acc:
+#         print(acc['client_name', 'balance'])
+
+# name_balance(accounts)
---
+++ week-02/day-03/he_will_never.py
@@ -0,0 +1,24 @@
+# Things are a little bit messed up
+# Your job is to decode the not_so_cryptic_message by using the hashmap as a look up table
+# Assemble the fragments into the out variable
+
+out = ""
+not_so_cryptic_message = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11]
+
+hashmap = {
+    7: "run around and desert you",
+    50: "tell a lie and hurt you",
+    49: "make you cry,",
+    2: "let you down",
+    12: "give you up,",
+    1: "Never gonna",
+    11: "\n",
+    3: "say goodbye"
+}
+
+
+
+
+
+print(hashmap)
+print(out)
