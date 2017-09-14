a9db8edef9bfb58c8195e122e036b3dc0dbfb2ed
András Kamocsai <kamo.andras@gmail.com>
2017-09-14 11:03:39 +0200

    feat(element_finder): done

---
+++ week-02/day-03/element_finder.py
@@ -0,0 +1,8 @@
+# Check if the list contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+
+numbers = [1, 2, 3, 4, 5, 6, 8]
+
+if 7 in numbers:
+    print("Hoorray")
+else:
+    print("Nooooo!")
