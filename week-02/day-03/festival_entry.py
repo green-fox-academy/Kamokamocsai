3db7ef574d4ac8910901c2892ff5b600f4b530be
András Kamocsai <kamo.andras@gmail.com>
2017-09-14 16:10:35 +0200

    feat(festival_entry): alcohol and guns 0 but not removed the dangerous elements

---
+++ week-02/day-03/festival_entry.py
@@ -0,0 +1,44 @@
+
+watchlist = []
+
+security_alcohol_loot = 0
+
+queue = [
+	{ 'name': 'Amanda', 'alcohol': 10, 'guns': 1 },
+	{ 'name': 'Tibi', 'alcohol': 0, 'guns': 0 },
+	{ 'name': 'Dolores', 'alcohol': 0, 'guns': 1 },
+	{ 'name': 'Wade', 'alcohol': 1, 'guns': 1 },
+	{ 'name': 'Anna', 'alcohol': 10, 'guns': 0 },
+	{ 'name': 'Rob', 'alcohol': 2, 'guns': 0 },
+	{ 'name': 'Joerg', 'alcohol': 20, 'guns': 0 }
+]
+
+# Queue of festivalgoers at entry
+# no. of alcohol units 
+# no. of guns
+
+# Create a security_check function that returns a list of festivalgoers who can enter the festival
+
+# If guns are found, remove them and put them on the watchlist (only the names)
+# If alcohol is found confiscate it (set it to zero and add it to security_alchol_loot) and let them enter the festival
+
+
+
+
+def security_check(queue):
+    for member in queue:
+        if member['guns'] > 0:
+            watchlist.append(member['name'])
+            member['guns'] = 0 
+            # queue.remove(member)
+        if member['guns'] == 0:
+            if member['alcohol'] > 0:
+                member['alcohol'] = 0
+
+
+
+    print(queue)
+
+security_check(queue)
+
+print(watchlist)
