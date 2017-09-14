4f0b706c2c76aa69077d74439c8df40299a06f5e
András Kamocsai <kamo.andras@gmail.com>
2017-09-14 10:32:36 +0200

    feat(append_letter): done

---
+++ week-02/day-03/append_letter.py
@@ -0,0 +1,16 @@
+# Create a function called 'create_new_verbs()' which takes a list of verbs and a string as parameters
+# The string shouldf be a preverb
+# The function appends every verb to the preverb and returns the list of the new verbs
+
+
+verbs = ["megy", "ver", "kapcsol", "rak", "néz"]
+preverb = "be"
+
+def create_new_verbs(preverb):
+    for i in range(len(verbs)):
+        verbs[i] = preverb + verbs[i]
+    return verbs
+
+create_new_verbs(preverb)
+
+print(verbs)
