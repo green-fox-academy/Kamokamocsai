ed29458b8b273d1aa1c936d40df051755fbd71e7
András Kamocsai <kamo.andras@gmail.com>
2017-09-14 09:50:36 +0200

    feat(solar_system): done

---
+++ week-02/day-03/solar_system.py
@@ -0,0 +1,8 @@
+# Saturn is missing from the planet_list
+# Insert it into the correct position
+
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune"]
+
+planet_list.insert(5, "Saturn")
+
+print(planet_list)
