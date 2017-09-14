d8f2e5604ab84d369c247c97ce9552f74d37b7c0
András Kamocsai <kamo.andras@gmail.com>
2017-09-14 09:38:43 +0200

    feat(he_will_never): done

--- week-02/day-03/he_will_never.py
+++ week-02/day-03/he_will_never.py
@@ -8,17 +8,16 @@ not_so_cryptic_message = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11]
 hashmap = {
     7: "run around and desert you",
     50: "tell a lie and hurt you",
-    49: "make you cry,",
+    49: "make you cry, ",
     2: "let you down",
-    12: "give you up,",
-    1: "Never gonna",
+    12: "give you up, ",
+    1: "Never gonna ",
     11: "\n",
     3: "say goodbye"
 }
 
 
+for i in not_so_cryptic_message:
+    out += hashmap[i]
 
-
-
-print(hashmap)
 print(out)
