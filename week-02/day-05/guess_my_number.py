3438b56905096ed62f81022379484837a5859302
András Kamocsai <kamo.andras@gmail.com>
2017-09-15 11:51:15 +0200

    feat(guess_my_number): done

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -2,18 +2,20 @@ from random import randint
 
 random_number = randint(1, 100)
 
-user_life = 5
 user_guess = int(0)
 
 def guessed(user_guess):
+    user_life = 20
     while not user_guess == random_number:
         user_guess = int(input("Pick a number between 1 and 100: "))
         if user_guess == random_number:
             print("Congratulations! You won!")
         elif user_guess > random_number:
-            print("Too high! You have x lives left.")
+            user_life = user_life - 1                    
+            print("Too high! You have " + str(user_life) + " lives left.")
         elif user_guess < random_number:
-            print("Too low! You have x lives left.")
+            # user_life -= 1
+            print("Too low! You have " + str(user_life) + " lives left.")
         
 
 guessed(user_guess)
