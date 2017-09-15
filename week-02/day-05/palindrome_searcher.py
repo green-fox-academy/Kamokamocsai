e9086a6849a69ef8da68472a0636995a371f4015
András Kamocsai <kamo.andras@gmail.com>
2017-09-15 15:10:04 +0200

    feat(palindrome_searcher): its write everything

--- week-02/day-05/palindrome_searcher.py
+++ week-02/day-05/palindrome_searcher.py
@@ -1,14 +1,20 @@
-user_input = input("Add words to search palindromes:")
+user_input = input("Add words and search palindromes:")
 
 
 def search_palindrome(user_input):
-    for i in range (0, (len(user_input) + 1) / 2):
-        if user_input[i] != user_input[i]:
-            return False
-    return True      
+    output = []
+    for i in range(len(user_input)-1, 0, -1):
+        for j in range(i):
+            x = user_input[j:i]
+            if x == x[::-1]:
+            # print(user_input[j])
+                output.append(x)  
+                    
+        print(output)
     
 search_palindrome(user_input)
 
-output = []
-
-print(output)
+# def create_palindrome(user_input, user_input_reversed):
+#     palindrome = user_input + user_input_reversed
+#     print(palindrome)
+# create_palindrome(user_input, user_input_reversed)
