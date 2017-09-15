9e4565e555aaf8b27b84203b3dbb88ab72c3866c
András Kamocsai <kamo.andras@gmail.com>
2017-09-15 11:43:18 +0200

    feat(guess_my_number): the number guess is works

--- week-02/day-05/guess_my_number.py
+++ week-02/day-05/guess_my_number.py
@@ -6,13 +6,14 @@ user_life = 5
 user_guess = int(0)
 
 def guessed(user_guess):
-    user_guess = int(input("Pick a number between 1 and 100: "))
-    if user_guess == random_number:
-        print("Congratulations! You won!")
-    elif user_guess > random_number:
-        print("Too high! You have x lives left.")
-    elif user_guess < random_number:
-        print("Too low! You have x lives left.")
-    return user_guess
+    while not user_guess == random_number:
+        user_guess = int(input("Pick a number between 1 and 100: "))
+        if user_guess == random_number:
+            print("Congratulations! You won!")
+        elif user_guess > random_number:
+            print("Too high! You have x lives left.")
+        elif user_guess < random_number:
+            print("Too low! You have x lives left.")
+        
 
 guessed(user_guess)
