8cfb254c997dd213c95ec06d37911997ca26a56f
András Kamocsai <kamo.andras@gmail.com>
2017-09-20 14:55:43 +0200

    feat(purple_steps_3): done

---
+++ week-03/day-03/purple_steps_3.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root = Tk()
+
+canvas = Canvas(root, width='300', height='300')
+canvas.pack()
+
+
+def draw_square(square_size):
+    x_pos = 0
+    y_pos = 0
+    while x_pos <= 300:
+        canvas.create_rectangle(x_pos + square_size, y_pos + square_size , x_pos + square_size * 2, y_pos + square_size * 2, fill='purple')
+        x_pos += square_size / 100
+        y_pos += square_size / 100
+        square_size *= 2
+
+draw_square(10)
+
+
+root.mainloop()
