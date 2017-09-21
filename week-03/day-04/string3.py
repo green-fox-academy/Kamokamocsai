66683b51513f4ca9d6ec94e968109c66c10670da
András Kamocsai <kamo.andras@gmail.com>
2017-09-21 16:09:23 +0200

    feat(string3): done

--- week-03/day-04/string3.py
+++ week-03/day-04/string3.py
@@ -1,2 +1,12 @@
 # Given a string, compute recursively a new string where all the
 # adjacent chars are now separated by a "*".
+
+def string_checker(string):
+    if string == "":
+        return ""
+    elif string[0] == " ":
+        return "*" + string_checker(string[1:])
+    else: 
+        return string[0] + string_checker(string[1:])
+        
+print(string_checker("kmoegn jxnckjb isbcbccbcxxubix"))
