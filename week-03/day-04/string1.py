d42b835a2846e7597ec84ec2302a0ff05c9641b9
András Kamocsai <kamo.andras@gmail.com>
2017-09-21 14:47:53 +0200

    feat(string1): done

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,2 +1,13 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+
+def string_checker(string):
+    if string == "":
+        return ""
+    elif string[0] == "x":
+        return "y" + string_checker(string[1:])
+    else: 
+        return string[0] + string_checker(string[1:])
+        
+
+print(string_checker("kmoegnjxnckjbisbcbccbcxxubix"))
