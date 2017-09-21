3224b090dd67326c749beb5bc98d9c987ab2f3dc
András Kamocsai <kamo.andras@gmail.com>
2017-09-21 11:51:32 +0200

    feat(power): done

--- week-03/day-04/power.py
+++ week-03/day-04/power.py
@@ -1,2 +1,11 @@
 # Given base and n that are both 1 or more, compute recursively (no loops)
 # the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+def power_number(base, number):
+    if number == 1:
+        return base * number
+    else: 
+        return base * power_number(base, number - 1)
+
+
+print(power_number(6, 6))
