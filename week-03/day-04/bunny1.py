a9affbf6084721230ac15577151883f4b7c535ae
András Kamocsai <kamo.andras@gmail.com>
2017-09-21 12:09:22 +0200

    feat(bunny1): done

--- week-03/day-04/bunny1.py
+++ week-03/day-04/bunny1.py
@@ -1,2 +1,11 @@
 # We have a number of bunnies and each bunny has two big floppy ears.
 # We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
+
+def total_bunny_ears(bunnies):
+    ears = 2
+    if bunnies == 0:
+        return bunnies
+    else:
+        return ears + total_bunny_ears(bunnies - 1)
+
+print(total_bunny_ears(20))
