468201ccbcef4a0b47ef6a95477b6009c85d1d83
András Kamocsai <kamo.andras@gmail.com>
2017-09-21 16:08:02 +0200

    feat(string2): x remover working

--- week-03/day-04/string2.py
+++ week-03/day-04/string2.py
@@ -1,1 +1,13 @@
 # Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+def string_checker(string):
+    if string == "":
+        return ""
+    elif string[0] == "x":
+        return "" + string_checker(string[1:])
+    else: 
+        return string[0] + string_checker(string[1:])
+        
+
+
+print(string_checker("kmoegnjxnckjbisbcbccbcxxubix"))
