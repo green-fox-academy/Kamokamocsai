2587772e0df9b3912705f4504ec66723dfe13eda
András Kamocsai <kamo.andras@gmail.com>
2017-09-23 00:50:05 +0200

    feat(fractals): draw fractals with random colors

--- week-03/day-05/triangle_fractal.py
+++ week-03/day-05/triangle_fractal.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import time
 import math
+import random
 
 root = Tk()
 
@@ -11,12 +12,12 @@ size = 590
 
 
 def draw_triangle(x, y, size):
+    r = lambda: random.randint(0, 255)
+    random_color = '#%02X%02X%02X' % (r(),r(),r())
     canvas.create_polygon(x, y, 
                           x + size, y,
                           x + (size / 2), y + size,
-                          outline='black', fill='')
-
-
+                          outline='black', fill=random_color)
 
 
 def draw_fractal(x, y, size):
@@ -29,6 +30,7 @@ def draw_fractal(x, y, size):
         draw_fractal(x, y, size / 2)
         draw_fractal(x + size / 2, y, size / 2)
         draw_fractal(x + size / 4, y + size / 2, size / 2)
+
     
 draw_fractal(10, 10, size)
 root.mainloop()
