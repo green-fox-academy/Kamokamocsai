f99cede8f441cdd2f8e6fa13d72ec681807e4eb8
András Kamocsai <kamo.andras@gmail.com>
2017-09-25 16:34:24 +0200

    feat(teacher_and_students): created py file and comments

---
+++ week-04/day-01/teacher_student.py
@@ -0,0 +1,7 @@
+# Create Student and Teacher classes
+# Student
+# learn()
+# question(teacher) -> calls the teachers answer method
+# Teacher
+# teach(student) -> calls the students learn method
+# answer()
