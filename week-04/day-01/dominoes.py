f41f6618c33da24ebaffee09d3f821bd21e93a31
András Kamocsai <kamo.andras@gmail.com>
2017-09-25 12:42:26 +0200

    feat(dominoes): add two for loops for order of the dominoes

---
+++ week-04/day-01/dominoes.py
@@ -0,0 +1,28 @@
+from domino import Domino
+
+def initialize_dominoes():
+    dominoes = []
+    dominoes.append(Domino(5, 2))
+    dominoes.append(Domino(4, 6))
+    dominoes.append(Domino(1, 5))
+    dominoes.append(Domino(6, 7))
+    dominoes.append(Domino(2 ,4))
+    dominoes.append(Domino(7, 1))
+    return dominoes
+
+dominoes = initialize_dominoes()
+# You have the list of Dominoes
+# Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
+# eg: [2, 4], [4, 3], [3, 5] ...
+
+class orders_of_dominoes(object):
+    def __init__(self, value_one, value_two):
+        for i in range(len(dominoes)):
+            for j in range(len(dominoes)):
+                print(i)
+                print(j)
+                # if i.values[1] == j.values[0]:
+                #     dominoes.append(i)
+        
+
+print(dominoes.orders_of_dominoes)
