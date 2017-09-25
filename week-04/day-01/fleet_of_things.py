d28f51e07480a7b875e886ae2eeecbb4b0a06f40
András Kamocsai <kamo.andras@gmail.com>
2017-09-25 15:40:44 +0200

    feat(fleet_of_things): first thing done

---
+++ week-04/day-01/fleet_of_things.py
@@ -0,0 +1,15 @@
+from fleet import Fleet
+from thing import Thing
+
+fleet = Fleet()
+# Create a fleet of things to have this output:
+# 1. [ ] Get milk
+# 2. [ ] Remove the obstacles
+# 3. [x] Stand up
+# 4. [x] Eat lunch
+
+thing1 = Thing("Get milk")
+print(thing1)
+
+
+print(fleet)
