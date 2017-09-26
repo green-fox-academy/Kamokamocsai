6c2d8e3f43efb594d247abecfddcd27110463fd3
András Kamocsai <kamo.andras@gmail.com>
2017-09-26 12:23:52 +0200

    feat(green_fox_python): done

--- week-04/day-02/green_fox_python.py
+++ week-04/day-02/green_fox_python.py
@@ -116,7 +116,7 @@ for member in people:
     member.introduce()
     member.get_goal()
 
-badass = LagopusClass('BADA55')
+badass = PallidaClass('24bb1t')
 badass.add_student(student)
 badass.add_student(john)
 badass.add_mentor(mentor)
