b9ebca4380dfc03171df213414ea3c07eb3e213b
András Kamocsai <kamo.andras@gmail.com>
2017-09-26 14:57:02 +0200

    feat(checkerboard, teacher_student): nothing changes

--- week-03/day-03/checkerboard.py
+++ week-03/day-03/checkerboard.py
@@ -1,19 +1,20 @@
+# fill the canvas with a checkerboard pattern.
 from tkinter import *
 
 root = Tk()
 
-canvas = Canvas(root, width='300', height='300')
+canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
-# fill the canvas with a checkerboard pattern.
-
-cell_size = 300 / 8
+cell_size = 600 / 8
 
 def draw_checkerboard():
     for row in range(8):             
         for column in range(8):           
             if (row + column) % 2 == 0:     
-                canvas.create_rectangle(column * cell_size, row * cell_size, column * cell_size + cell_size, row * cell_size + cell_size, fill='black')
+                canvas.create_rectangle(column * cell_size, row * cell_size, 
+                                        column * cell_size + cell_size, 
+                                        row * cell_size + cell_size, fill='black')
 
 draw_checkerboard()
 
--- week-04/day-01/teacher_student.py
+++ week-04/day-01/teacher_student.py
@@ -5,3 +5,18 @@
 # Teacher
 # teach(student) -> calls the students learn method
 # answer()
+
+class Student(object):
+    def learn():
+        pass
+
+    def question(teacher):
+        pass
+
+
+class Teacher(object):
+    def teach(student):
+        pass
+
+    def answer():
+        pass
