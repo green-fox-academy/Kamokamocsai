423edd153d07aecf4591311a1f2a18a37d242e55
András Kamocsai <kamo.andras@gmail.com>
2017-09-27 15:49:57 +0200

    feat(sum list numbers): done

--- week-04/day-03/sum_test.py
+++ week-04/day-03/sum_test.py
@@ -1,1 +1,19 @@
-testsums
+import unittest
+from summ import SumNumbers
+
+test_summ = SumNumbers()
+
+class SumNumbersTest(unittest.TestCase):
+    
+    def test_empty(self):
+        self.assertEqual(test_summ.sum_numbers([]), 0)
+
+    def test_empty(self):
+        self.assertEqual(test_summ.sum_numbers([1, 3, 6]), 10)
+
+    def test_empty(self):
+        self.assertNotEqual(test_summ.sum_numbers([2, 2]), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ week-04/day-03/summ.py
@@ -0,0 +1,7 @@
+class SumNumbers(object):
+    
+    def sum_numbers(self, numbers):
+        summ = 0
+        for number in numbers:
+            summ += number
+        return summ
