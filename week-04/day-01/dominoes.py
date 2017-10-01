ade623dcc26fdfed43dc4eca94ee97fe3d87d24e
András Kamocsai <kamo.andras@gmail.com>
2017-10-01 21:15:19 +0200

    refactor(dominoes): refactored for demonstration

--- week-04/day-01/dominoes.py
+++ week-04/day-01/dominoes.py
@@ -15,8 +15,8 @@ dominoes = initialize_dominoes()
 # Order them into one snake where the adjacent dominoes have the same numbers on their adjacent sides
 # eg: [2, 4], [4, 3], [3, 5] ...
 
-for domino in range(len(dominoes)):
-    for domino_value in range(1,len(dominoes)):
-        if dominoes[domino].values[1] == dominoes[domino_value].values[0]:
-            dominoes[domino+1], dominoes[domino_value] = dominoes[domino_value], dominoes[domino+1]
+for i in range(len(dominoes)):
+    for j in range(1,len(dominoes)):
+        if dominoes[i].values[1] == dominoes[j].values[0]:
+            dominoes[i+1], dominoes[j] = dominoes[j], dominoes[i+1]
 print(dominoes)
