b9701a4a9b878db578c88e0c6985619cb87ceac7
András Kamocsai <kamo.andras@gmail.com>
2017-10-02 15:33:22 +0200

    feat(rpg game): floor drawing and hero moving

--- week-05/rpg_game/resource/view.py
+++ week-05/rpg_game/resource/view.py
@@ -1,35 +1,49 @@
 from tkinter import *
 
-size = 600
+size = 720
 
 root = Tk()
 root.configure(background ='black')
 root.title("Wanderer - RPG Game by Kamo")
-canvas = Canvas(root, width=size, height=size,bg="yellow",bd=0)
+canvas = Canvas(root, width=size, height=size, bg="yellow", bd=0)
 canvas.pack()
+
+
 
 class App:
     def __init__(self):
         self.rect = None
+        self.floor = PhotoImage(file = "img/floor.png")
+        self.draw_map()
 
-
-
-
-    def base_shape(self, x, y, size):
+    def hero(self, x, y, size):
         self.rect = canvas.create_rectangle(x, y, x+size, y+size, fill = "black", width=0)
 
     def move(self, dx, dy):
         canvas.move(self.rect, dx, dy )
 
+    def draw_map(self):
+        cell_size = size / 10
+        for row in range(10):             
+            for column in range(10): 
+                cell = canvas.create_image(column * cell_size + 2, row * cell_size + 2, image = self.floor, anchor = 'nw')
+
+
+
 myApp = App()
 
-myApp.base_shape(0, 0, 100)
+myApp.hero(0, 0, 72)
 
 def on_key_press(e):
     if ( e.keysym == 'Up' ):
-        myApp.move(0,-10)
+        myApp.move(0,-72)
     elif( e.keysym == 'Down' ):
-        myApp.move(0,10)
+        myApp.move(0,72)
+    elif( e.keysym == 'Left' ):
+        myApp.move(-72,0)
+    elif( e.keysym == 'Right' ):
+        myApp.move(72,0)
+
 
 # Tell the canvas that we prepared a function that can deal with the key press events
 root.bind("<KeyPress>", on_key_press)
