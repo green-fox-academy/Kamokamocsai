5fa2a6fdd1f27a9da58fb6775211a68e668d3781
András Kamocsai <kamo.andras@gmail.com>
2017-10-02 18:02:43 +0200

    feat(rpg game): floors and walls is completed

--- week-05/rpg_game/main.py
+++ week-05/rpg_game/main.py
@@ -3,7 +3,13 @@ from resource import map_lvl
 class GameProcess(object):
 
     def __init__(self):
-        pass
+        self.map_level = map_level
+        self.running = running
+        self.level = level
+        self.entities = []
+        self.hero = hero
 
     def keyboard_handler(self):
         pass
+        # call_hero_move
+        # call_all_entities
--- week-05/rpg_game/resource/view.py
+++ week-05/rpg_game/resource/view.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from map_lvl import map1
+
 
 size = 720
 
@@ -13,27 +15,39 @@ canvas.pack()
 class App:
     def __init__(self):
         self.rect = None
-        self.hero_img = PhotoImage(file = "img/hero-down.png")
+        # self.hero_img = PhotoImage(file = "img/hero-down.png")
+        self.hero_down = PhotoImage(file = "img/hero-down.png")
+        self.hero_left = PhotoImage(file = "img/hero-left.png")
+        self.hero_right = PhotoImage(file = "img/hero-right.png")
+        self.hero_up = PhotoImage(file = "img/hero-up.png")
         self.floor = PhotoImage(file = "img/floor.png")
+        self.wall = PhotoImage(file = "img/wall.png")
         self.draw_map()
 
-    def hero(self, x, y, size):
-        self.rect = canvas.create_image(x, y, image = self.hero_img, anchor= 'nw')
+    def hero(self, x, y, hero_img):
+        self.rect = canvas.create_image(x, y, image = hero_img, anchor= 'nw')
+
 
     def move(self, dx, dy):
-        canvas.move(self.rect, dx, dy )
+        
+        canvas.move(self.rect, dx, dy)
 
     def draw_map(self):
         cell_size = size / 10
-        for row in range(10):             
-            for column in range(10): 
-                cell = canvas.create_image(column * cell_size + 2, row * cell_size + 2, image = self.floor, anchor = 'nw')
+        for row in range(len(map1)):             
+            for column in range(len(map1)): 
+                if map1[row][column] == 1:
+                    cell = canvas.create_image(column * cell_size + 2, row * cell_size + 2, image = self.floor, anchor = 'nw')
+                    
+                else:
+                    canvas.create_image(column * cell_size + 2, row * cell_size + 2, image = self.wall, anchor = 'nw')
+                    
 
 
 
 myApp = App()
 
-myApp.hero(0, 0, 72)
+myApp.hero(0, 0, myApp.hero_down)
 
 def on_key_press(e):
     if ( e.keysym == 'Up' ):
