f854e4a81be420dcd6e3b49f85a6ba34c17014fc
András Kamocsai <kamo.andras@gmail.com>
2017-10-24 15:09:18 +0200

    feat(i wont cheat on the exam): done

---
+++ week-07/day-02/15 i-wont-cheat-on-the-exam.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let quote = "I won't cheat on the exam!";
+
+console.log(quote.repeat(100));
