131f3ad51c4c4e7898ce910e3a6f634067f79dcb
András Kamocsai <kamo.andras@gmail.com>
2017-10-24 15:31:08 +0200

    feat(fizz-buzz): done

---
+++ week-07/day-02/18 fizz-buzz.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+
+for (let number = 1; number <= 100; number++) {
+    if (number % 3 === 0) {
+        console.log('Fizz');
+    } else if (number % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(number);
+    }
+}
