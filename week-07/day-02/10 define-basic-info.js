8621a5172e0da35f839e66f250674bd5a271e19a
András Kamocsai <kamo.andras@gmail.com>
2017-10-24 12:02:36 +0200

    feat(define-basic-info.js): done

---
+++ week-07/day-02/10 define-basic-info.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = 'Kamo';
+let age = 34;
+let height = 1.73;
+let married = true;
+
+console.log(name + " " + age + " " + height + " " + married)
