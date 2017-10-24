165c1f937053bb04d3ea1c97c2a2d36218311f0b
András Kamocsai <kamo.andras@gmail.com>
2017-10-24 11:27:40 +0200

    feat(hello-others): done

---
+++ week-07/day-02/03hello-others.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+
+console.log('Hello, Aaki baacsi!')
+console.log('Hello, Andris!')
+console.log('Hello, Peeter!')
