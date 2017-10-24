1a5c0b3b5de87fee4738bfe14ec1f5f98ae3e83f
András Kamocsai <kamo.andras@gmail.com>
2017-10-24 13:38:22 +0200

    feat(cuboid): done

---
+++ week-07/day-02/cuboid.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Write a program that stores 3 sides of a cuboid as variables (floats)
+// The program should write the surface area and volume of the cuboid like:
+//
+// Surface Area: 600
+// Volume: 1000
+
+let a = 10;
+let b = 15;
+let c = 20;
+
+let surfaceArea = 2 * c * (a + b);
+let surfaceVolume = a * b * c;
+console.log(surfaceArea);
+console.log(surfaceVolume);
