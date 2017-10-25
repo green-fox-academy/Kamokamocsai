827e127131d07a38b93194f8dfb1732433fbe096
András Kamocsai <kamo.andras@gmail.com>
2017-10-25 14:25:05 +0200

    feat(reverse): done

---
+++ week-07/day-03/01/02 reverse.js
@@ -0,0 +1,11 @@
+'use strict';
+// Reverse the string!
+
+var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+function reverse(words){
+    return words.split("").reverse().join("");
+    return words;
+}
+
+console.log(reverse (reversed));
