479cc08d46609818c791edf814bd552f0bb6eb4c
András Kamocsai <kamo.andras@gmail.com>
2017-10-25 11:50:11 +0200

    feat(append-a): done

---
+++ week-07/day-03/03 append-a.js
@@ -0,0 +1,13 @@
+'use strict';
+// - Create a variable named `am` and assign the value `kuty` to it
+// - Write a function called `appendA` that gets a string as an input
+//   and appends an 'a' character to its end
+// - Print the result of `appendA(am)` to the console
+
+let am = 'kuty';
+
+function appendA (input) {
+    return input + 'a';
+}
+
+console.log(appendA(am));
