bc2d41a40a595d0921157670beccff151cfc5bba
András Kamocsai <kamo.andras@gmail.com>
2017-10-27 12:18:38 +0200

    feat(slider): layout complete

---
+++ week-07/day-05/slider.js
@@ -0,0 +1,29 @@
+let images = [{
+    'title': 'elso kep',
+    'description': "ide sok szoveg jon",
+    'url': "https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/1909758_705557362917651_5561632227437550336_n.jpg?oh=ce445a4b1be3161f7b8ff5424f090393&oe=5AA95D3B"
+},
+{
+    'title': 'masodik kep',
+    'description': 'sok-sok szoveg',
+    'url': "https://scontent-vie1-1.xx.fbcdn.net/v/t1.0-9/10325619_705557152917672_4269124930293393731_n.jpg?oh=36550dc35c38655bab9525c1f5d94216&oe=5A7F5A15"
+}
+];
+
+
+function renderThumbs(list) {
+    for (let imgIterator = 0; imgIterator < list.length; imgIterator++) {
+        console.log(images[imgIterator]['title']);
+    }
+    let thumbnail = document.querySelector('.thumbnail');
+    for (let i = 0; i < list.length; i++) {
+        let newListElement = document.createElement('img');
+        thumbnail.appendChild(newListElement);
+        newListElement.textContent = list[i]['title'];
+        let imgSrc = list[i]['url'];
+        newListElement.setAttribute('src', imgSrc);
+    } 
+    // thumbnail.appendChild
+}
+
+renderThumbs(images);
